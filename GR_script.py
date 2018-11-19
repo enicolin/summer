ed79ea4bc2856d52c85a319e2c69f78a8f93050e
Elia Nicolin <enic156@UoA.auckland.ac.nz>
2018-11-20 12:18:52 +1300

    updated how magnitudes are sampled by inverse transform sampling

--- GR_script.py
+++ GR_script.py
@@ -17,32 +17,35 @@ rnd.seed(a=1756)
 # determine the largest magnitude of these 100 eventsm Mmax. Repeat many times to 
 # estimate probability density function of Mmax.
 
-# define parameters a, b
+# define parameters
 a = 2
 b = 0.97
+Mc = 2.2
 
-Nt = 100 # total earthquakes to randomly sample at each iteration
-N = 750 # number of times to repeat the process
-Mmin = eq.GR_N(Nt, a, b) # given Nt, the smallest magnitude quake we expect to see given G-R
+Nt = 10**a # total earthquakes to randomly sample at each iteration
+n = int(7e3) # number of times to repeat the process
 
-Mmax = np.empty(N) # initialise array containing largest magnitude event at each iteration
-for i in range(N):
-    events = eq.sample_events(Nt) # array of length Nt containing elements uniformly random between 0 and Nt
+Mmax = np.empty(n) # initialise array containing largest magnitude event at each iteration
+for i in range(n):
+    events = eq.sample_magnitudes(Nt, Mc, b) # sample Nt magnitudes based off the distribution according to GR
     
-    # determine corresponding magnitudes for each event using G-R
-    magnitudes = np.empty(Nt)
-    for k in range(len(events)):
-        magnitudes[k] = eq.GR_N(events[k], a, b)
-        
-    Mmax[i] = magnitudes.max()
+#    # see what kind of distribution quakes are sampled from..
+#    plt.figure(1)
+#    plt.hist(events, color="darkblue", edgecolor = "black", bins = 100)
+#    plt.show()
+    
+    Mmax[i] = events.max()
+
 
 plt.figure(1)
 plt.hist(Mmax, color="red", edgecolor = "black", bins = 100)
-plt.title('Distribution of $M_{max}$')
+plt.title('Distribution of {} for {} events'.format("$M_{max}$", Nt))
 plt.xlabel('$M_{max}$')
 plt.ylabel('Occurences')
 plt.show()
 
-#plt.figure(2)
-#sns.distplot(Mmax, hist=True, kde=True, bins = 100, color = "black")
-#plt.show()
+plt.figure(2)
+sns.distplot(Mmax, hist=True, kde=True, bins = 100, color = "black")
+plt.title('kernel density estimation (mostly for visualisation right now)')
+plt.xlabel('$M_{max}$')
+plt.show()
--- eq_functions.py
+++ eq_functions.py
@@ -1,25 +1,37 @@
 import numpy as np
 import random as rnd
 
-def GR_M(M,a,b):
+def GR_M(M,a,b,Mc):
     # Use the Gutenberg-Richter law to give the number of events of at least magnitude M over a time period
     # (function of M)
     
-    N = 10**(a-b*M)
+    N = 10**(a-b*(M-Mc))
     return N
 
-def GR_N(N,a,b):
+def GR_N(N,a,b,Mc):
     # Given the number of events, use Gutenberg-Richter law to determine the magnitude of the smallest event
     # (function of N)
     
-    M = (1/b)*(a-np.log10(N))
+    M = (-1/b)*(np.log10(N)-a-b*Mc)
     return M
 
-def sample_events(Nt):
-    # create an array of length Nt with entries in the range [0,Nt]
-    events = np.empty(Nt)
-    for i in range(Nt):
-        events[i] = rnd.uniform(0.,float(Nt))
+def GR_inv(x,Mc,b):
+    # inverse of F, where F = F(x) = P(X<=x) - the probability of having X earthquakes less than magnitude x
+    # based off Gutenberg-Richter
+    # x is a uniformly random number on [0,1]
+    
+    Finv = Mc - (1/b)*np.log10(1-x)
+    return Finv
+
+def sample_magnitudes(n,Mc,b):
+    # sample n earthquake events given appropriate parameters based off GR
+    # uses the probability integral transform method
+    # returns array of length n, whose ith element is the magnitude of the ith event
+    
+    events = np.empty(n) # initialise 
+    for i in range(n):
+        xi = rnd.uniform(0,1) # pseudorandom number on [0,1] from a uniform distribution
+        events[i] = GR_inv(xi, Mc, b)
         
     return events
 
