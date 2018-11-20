35666170d3407a7464abaa852a7c4a4e1a1359ae
Elia Nicolin <enic156@UoA.auckland.ac.nz>
2018-11-21 12:45:21 +1300

    added chi square test

--- test_poisson.py
+++ test_poisson.py
@@ -11,24 +11,68 @@ import eq_functions as eq
 import random as rnd
 import numpy as np
 from matplotlib import pyplot as plt
+from collections import Counter
+from scipy.stats import chisquare
 
 rnd.seed(a=1756)
 
-#lmbd = 1
+nsamples = int(1e5)
 
 
 for lmbd in range(1,15):
-    rand1 = eq.sample_poisson(lmbd,2000)
+    # alternate colour of plots for easier readability
+    if lmbd % 2 == 0:
+        colour = "darkblue"
+    else:
+        colour = "red"
     
-    rand2 = np.random.poisson(lmbd,2000)
+    poiss_mine = eq.sample_poisson(lmbd,nsamples)
+    
+    poiss_builtin = np.random.poisson(lmbd,nsamples)
     
     fig, axes = plt.subplots(2, 1)
     
-    axes[0].hist(rand1,color="darkblue",edgecolor="black",bins=list(np.linspace(0,int(rand1.max()),int(rand1.max())+1)))
-    axes[0].set_title('own sampler, {} = {}'.format("$\lambda$",lmbd))
-    axes[0].set_xlim([0,rand1.max()])
-    axes[1].hist(rand2,color="darkblue",edgecolor="black",bins=list(np.linspace(0,int(rand2.max()),int(rand2.max())+1)))
-    axes[1].set_title('built-in sampler, {} = {}'.format("$\lambda$",lmbd))
-    axes[1].set_xlim([0,rand2.max()])
+    xlim = max(poiss_builtin.max(),poiss_mine.max()) # x limit for equal scale on both subplots
+    
+    # chi-squared test for how poisson my sampler is
+
+    # get frequency of observations for each poisson number
+    n_poissnumbers = int(poiss_mine.max()) + 1 # number of poisson numbers
+    f_obs = np.zeros(n_poissnumbers) # array to store frequency of observations of each sampled number
+    obs_counter = Counter(poiss_mine) # count occurences of each sampled poisson number
+    for i in range(n_poissnumbers): 
+        f_obs[i] = obs_counter[i] # in the ith position, store the number of occurences of the ith poisson number
+    
+    # get frequency of expected observations for each poisson number
+    f_exp = np.zeros(n_poissnumbers)
+    for i in range(n_poissnumbers):
+        f_exp[i] = nsamples * np.exp(-lmbd)*(lmbd**i)/(np.math.factorial(i))
+    
+    chisq_mine, p_mine = chisquare(f_obs,f_exp,ddof=2)
+    
+    axes[0].hist(poiss_mine,color=colour,edgecolor="black",bins=list(np.linspace(0,int(poiss_mine.max()),int(poiss_mine.max())+1)))
+    axes[0].set_title('own sampler, {} = {}, p-value: {}'.format("$\lambda$", lmbd, p_mine))
+    axes[0].set_xlim([0,xlim])
+    
+    # chi-squared test for how poisson the built in sampler is
+
+    # get frequency of observations for each poisson number
+    n_poissnumbers = int(poiss_builtin.max()) + 1 # number of poisson numbers
+    f_obs = np.zeros(n_poissnumbers) # array to store number of observations of each sampled number
+    obs_counter = Counter(poiss_builtin) # count occurences of each sampled poisson number
+    for i in range(n_poissnumbers): 
+        f_obs[i] = obs_counter[i] # in the ith position, store the number of occurences of the ith poisson number
+    
+    # not super efficient, repeating some stuff uneccessarily here
+    # get frequency of expected observations for each poisson number
+    f_exp = np.zeros(n_poissnumbers)
+    for i in range(n_poissnumbers):
+        f_exp[i] = nsamples * np.exp(-lmbd)*(lmbd**i)/(np.math.factorial(i))
+    
+    chisq_builtin, p_builtin = chisquare(f_obs,f_exp,ddof=2)
+    
+    axes[1].hist(poiss_builtin,color=colour,edgecolor="black",bins=list(np.linspace(0,int(poiss_builtin.max()),int(poiss_builtin.max())+1)))
+    axes[1].set_title('built-in sampler, {} = {}, p-value = {}'.format("$\lambda$", lmbd, p_builtin))
+    axes[1].set_xlim([0,xlim])
     plt.tight_layout()
     plt.show()
