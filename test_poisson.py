a68b4b2d3408552d4364a82f4903b8c8190efa3a
Elia Nicolin <enic156@UoA.auckland.ac.nz>
2018-11-21 09:23:40 +1300

    more graphical comparisons to a built-in sampler

--- test_poisson.py
+++ test_poisson.py
@@ -14,19 +14,21 @@ from matplotlib import pyplot as plt
 
 rnd.seed(a=1756)
 
-lmbd = 10
+#lmbd = 1
 
-rand1 = eq.sample_poisson(lmbd,2000)
 
-rand2 = np.random.poisson(lmbd,2000)
-
-fig, axes = plt.subplots(2, 1)
-
-axes[0].hist(rand1,color="darkblue",edgecolor="black",bins=50)
-axes[0].set_title('own sampler')
-axes[0].set_xlim([0,16])
-axes[1].hist(rand2,color="darkblue",edgecolor="black",bins=50)
-axes[1].set_title('built-in sampler')
-axes[1].set_xlim([0,16])
-plt.tight_layout()
-plt.show()
+for lmbd in range(1,15):
+    rand1 = eq.sample_poisson(lmbd,2000)
+    
+    rand2 = np.random.poisson(lmbd,2000)
+    
+    fig, axes = plt.subplots(2, 1)
+    
+    axes[0].hist(rand1,color="darkblue",edgecolor="black",bins=int(3*rand1.max()))
+    axes[0].set_title('own sampler, {} = {}'.format("$\lambda$",lmbd))
+    axes[0].set_xlim([0,20])
+    axes[1].hist(rand2,color="darkblue",edgecolor="black",bins=int(3*rand2.max()))
+    axes[1].set_title('built-in sampler, {} = {}'.format("$\lambda$",lmbd))
+    axes[1].set_xlim([0,20])
+    plt.tight_layout()
+    plt.show()
