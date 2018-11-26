a92f759b2a8273f2d21dcfcc9fdc053d56ef6c59
Elia Nicolin <enic156@UoA.auckland.ac.nz>
2018-11-26 16:30:33 +1300

    implemented something for the plotting of the catalog

--- eqcatalogue.py
+++ eqcatalogue.py
@@ -8,8 +8,8 @@ np.random.seed(1756)
 rnd.seed(1756)
 
 # define parameters
-Nt = 100
-Tf = 12 # unit time
+Nt = 400
+Tf = 32 # unit time
 a = np.log10(Nt)
 b = 1.
 c = 1.
@@ -24,7 +24,7 @@ times = np.linspace(0,Tf,3*Tf+1) # time intervals
 dt = times[1]-times[0]# define time increment
 
 # intended column order
-cols = ['Average aftershock frequency','Events','Magnitude','Distance','Time']
+cols = ['n_avg','Events','Magnitude','Distance','theta','Time']
 events_occured = 0 # number of earthquakes generated 
 for t in times[:-1]: # for each time interval
     # average seismicity rate on interval [t,t+dt]
@@ -37,6 +37,7 @@ for t in times[:-1]: # for each time interval
     mgtds = eq.sample_magnitudes(X, Mc, b)
     
     distances = eq.sample_location(X, cprime, pprime)
+    thetas = np.random.uniform(0, 2*np.pi, X)
     
     # generate the times at which each event occurs, according to an exponential distribution.
     # the parameter for the exponential distribution at time interval [t,t+dt] is the expected number
@@ -63,9 +64,10 @@ for t in times[:-1]: # for each time interval
             n_avgcol[0] = n_avg # only include average number of events on first row
             catalog = pd.DataFrame({'Magnitude': mgtds,
                                    'Events':Xcol,
-                                   'Average aftershock frequency':n_avgcol,
+                                   'n_avg':n_avgcol,
                                    'Distance':distances,
-                                   'Time':times}, index = interval)
+                                   'Time':times,
+                                   'theta':thetas}, index = interval)
             catalog = catalog.reindex(columns = cols)
         else: # formatting for when there are no events during a time interval
             interval = ['Interval: [{:.2f},{:.2f}]'.format(t,t+dt)]
@@ -73,7 +75,8 @@ for t in times[:-1]: # for each time interval
                                       'X':[X],
                                       'n_avg':[n_avg],
                                       'Distance':['-'],
-                                      'Time':['-']}, index = interval)
+                                      'Time':['-'],
+                                      'theta':['-']}, index = interval)
             catalog = catalog.reindex(columns = cols)
     else: # join new results to existing catalog
         # index label for current time interval
@@ -86,21 +89,74 @@ for t in times[:-1]: # for each time interval
             n_avgcol[0] = n_avg
             catalog_update = pd.DataFrame({'Magnitude': mgtds,
                                       'Events':Xcol,
-                                      'Average aftershock frequency':n_avgcol,
+                                      'n_avg':n_avgcol,
                                       'Distance':distances,
-                                      'Time':times}, index = interval)
+                                      'Time':times,
+                                      'theta':thetas}, index = interval)
             catalog_update = catalog_update.reindex(columns = cols)
         else: # formatting for when there are no events during a time interval
             interval = ['Interval: [{:.2f},{:.2f}]'.format(t,t+dt)]
             catalog_update = pd.DataFrame({'Magnitude': ['-'],
                                       'Events':[X],
-                                      'Average aftershock frequency':[n_avg],
+                                      'n_avg':[n_avg],
                                       'Distance':['-'],
-                                      'Time':['-']}, index = interval)
+                                      'Time':['-'],
+                                      'theta':['-']}, index = interval)
             catalog_update = catalog_update.reindex(columns = cols)
         frames = [catalog, catalog_update]
         catalog = pd.concat(frames)
 
     events_occured += X
 
-#catalog.to_csv('catalog.csv')
+
+# plot the catalogue
+fig= plt.figure()
+ax = fig.add_subplot(111)#, projection = 'polar')
+fig.set_figheight(10)
+fig.set_figwidth(10)
+
+theta = catalog['theta']
+try:    
+    theta = theta.loc[theta != '-'] # when there are no intervals containing no events (marked by '-'), this comparison doesnt work. will look into better way of getting around it
+except TypeError:
+    pass
+theta = np.array(theta, dtype = np.float) # needs to be float 
+
+dist = catalog['Distance']
+try:
+    dist = dist.loc[dist != '-']
+except TypeError:
+    pass
+dist = np.array(dist, dtype = np.float)
+x = dist * np.cos(theta)
+y = dist * np.sin(theta)
+
+
+times = catalog['Time']
+try:
+    times = times.loc[times != '-']
+except TypeError:
+    pass
+times = np.array(times, dtype = np.float)
+
+magnitudes = catalog['Magnitude']
+try:
+    magnitudes = magnitudes.loc[magnitudes != '-']
+except TypeError:
+    pass
+magnitudes = np.array(magnitudes, dtype = np.float)
+
+plot = ax.scatter(x, y,
+           c = times,
+           s = 0.01*10**magnitudes, # large events much bigger than smaller ones
+           cmap = 'coolwarm')
+
+cbar = fig.colorbar(plot)
+cbar.set_label('Time')
+
+plt.title('Generated Events')
+plt.ylabel('y position')
+plt.xlabel('x position')
+plt.grid(True)
+plt.show()
+##catalog.to_csv('catalog.csv')
